72e20022da4b562258fdad7bb483a6d953d0f486
kumavis <aaron@kumavis.me>
2017-08-03 11:24:10 -0700

    test - aggregate test cases into lists

--- test/index.js
+++ test/index.js
@@ -9,172 +9,96 @@ test('basic test', (t) => {
 
   // blacklist
 
-  testDomain(t, {
-    domain: 'metamask.com',
-    type: 'blacklist',
-    expected: true,
-  })
-
-  testDomain(t, {
-    domain: 'myetherwaillet.com',
-    type: 'blacklist',
-    expected: true,
-  })
-
-  testDomain(t, {
-    domain: 'myetherwaller.com',
-    type: 'blacklist',
-    expected: true,
-  })
-
-  testDomain(t, {
-    domain: 'myetherweb.com.de',
-    type: 'blacklist',
-    expected: true,
-  })
-
-  testDomain(t, {
-    domain: 'myeterwallet.com',
-    type: 'blacklist',
-    expected: true,
-  })
+  testBlacklist(t, [
+    'metamask.com',
+    'myetherwaillet.com',
+    'myetherwaller.com',
+    'myetherweb.com.de',
+    'myeterwallet.com',
+    'xn--mytherwallet-fvb.com',
+  ])
 
   // whitelist
 
-  testDomain(t, {
-    domain: 'ledgerwallet.com',
-    type: 'whitelist',
-    expected: false,
-  })
-
-  // whitelist subdomains
-
-  testDomain(t, {
-    domain: 'www.metamask.io',
-    type: 'whitelist',
-    expected: false,
-  })
-
-  testDomain(t, {
-    domain: 'faucet.metamask.io',
-    type: 'whitelist',
-    expected: false,
-  })
-
-  testDomain(t, {
-    domain: 'zero.metamask.io',
-    type: 'whitelist',
-    expected: false,
-  })
-
-  testDomain(t, {
-    domain: 'zero-faucet.metamask.io',
-    type: 'whitelist',
-    expected: false,
-  })
+  testWhitelist(t, [
+    'ledgerwallet.com',
+    'metamask.io',
+    // whitelist subdomains
+    'www.metamask.io',
+    'faucet.metamask.io',
+    'zero.metamask.io',
+    'zero-faucet.metamask.io',
+    'www.myetherwallet.com',
+  ])
 
   // fuzzy
 
-  testDomain(t, {
-    domain: 'metmask.io',
-    type: 'fuzzy',
-    expected: true,
-  })
-
-  testDomain(t, {
-    domain: 'myetherwallet.cx',
-    type: 'fuzzy',
-    expected: true,
-  })
-
-  testDomain(t, {
-    domain: 'myetherwallet.aaa',
-    type: 'fuzzy',
-    expected: true,
-  })
-
-  testDomain(t, {
-    domain: 'myetherwallet.za',
-    type: 'fuzzy',
-    expected: true,
-  })
-
-  testDomain(t, {
-    domain: 'myetherwallet.z',
-    type: 'fuzzy',
-    expected: true,
-  })
+  testFuzzylist(t, [
+    'metmask.io',
+    'myetherwallet.cx',
+    'myetherwallet.aaa',
+    'myetherwallet.za',
+    'myetherwallet.z',
+  ])
 
   // no match
 
-  testDomain(t, {
-    domain: 'example.com',
-    type: 'all',
-    expected: false,
-  })
-
-  testDomain(t, {
-    domain: 'etherscan.io',
-    type: 'all',
-    expected: false,
-  })
-
-  testDomain(t, {
-    domain: 'ethereum.org',
-    type: 'all',
-    expected: false,
-  })
-
-  testDomain(t, {
-    domain: 'etherid.org',
-    type: 'all',
-    expected: false,
-  })
-
-  testDomain(t, {
-    domain: 'ether.cards',
-    type: 'all',
-    expected: false,
-  })
-
-  testDomain(t, {
-    domain: 'easyeth.com',
-    type: 'all',
-    expected: false,
-  })
-
-  testDomain(t, {
-    domain: 'etherdomain.com',
-    type: 'all',
-    expected: false,
-  })
-
-  testDomain(t, {
-    domain: 'ethnews.com',
-    type: 'all',
-    expected: false,
-  })
-
-  testDomain(t, {
-    domain: 'cryptocompare.com',
-    type: 'all',
-    expected: false,
-  })
-
-  testDomain(t, {
-    domain: 'kraken.com',
-    type: 'all',
-    expected: false,
-  })
-
-  testDomain(t, {
-    domain: 'myetherwallet.groovehq.com',
-    type: 'all',
-    expected: false,
-  })
+  testNoMatch(t, [
+    'example.com',
+    'etherscan.io',
+    'ethereum.org',
+    'etherid.org',
+    'ether.cards',
+    'easyeth.com',
+    'etherdomain.com',
+    'ethnews.com',
+    'cryptocompare.com',
+    'kraken.com',
+    'myetherwallet.groovehq.com',
+  ])
 
   t.end()
 })
+
+function testBlacklist(t, domains) {
+  domains.forEach((domain) => {
+    testDomain(t, {
+      domain: domain,
+      type: 'blacklist',
+      expected: true,
+    })
+  })
+}
+
+function testWhitelist(t, domains) {
+  domains.forEach((domain) => {
+    testDomain(t, {
+      domain: domain,
+      type: 'whitelist',
+      expected: false,
+    })
+  })
+}
+
+function testFuzzylist(t, domains) {
+  domains.forEach((domain) => {
+    testDomain(t, {
+      domain: domain,
+      type: 'fuzzy',
+      expected: true,
+    })
+  })
+}
+
+function testNoMatch(t, domains) {
+  domains.forEach((domain) => {
+    testDomain(t, {
+      domain: domain,
+      type: 'all',
+      expected: false,
+    })
+  })
+}
 
 function testDomain(t, { domain, type, expected }) {
   const value = detector.check(domain)
