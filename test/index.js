339201934ad812a127660e41ce4719d482a558b7
Simon Males <sime@sime.net.au>
2022-10-11 22:32:07 +0200

    Trezor 20221005 Phishing (#8744)
    * Trezor 20221005 Phishing
    
    * Trezor 20221005 Phishing
    
    * Trezor 20221005 Phishing
    
    * Trezor 20221005 Phishing
    
    * Trezor 20221005 Phishing
    
    Co-authored-by: Harry <409H@users.noreply.github.com>

--- test/index.js
+++ test/index.js
@@ -1255,7 +1255,7 @@ function testListIsPunycode (t, list) {
 function testListDoesntContainRepeats (t, list) {
   list.forEach((domain) => {
     const count = list.filter(item => item === domain).length
-    t.ok(count === 1, `domain "${domain}" appears in list only once`)
+    t.ok(count === 1, `domain "${domain}" is duplicated. Domains can only appear in list once`)
   })
 }
 
