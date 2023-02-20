0a1d3898783fbb4b0560499ea539bd4a86f8c5be
legobeat <109787230+legobeat@users.noreply.github.com>
2023-02-20 13:01:34 +0000

    Port main changes 20230220 (#11633)

---
+++ src/add-hosts.js
@@ -0,0 +1,96 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const config = require('./config.json');
+const PhishingDetector = require('./detector')
+
+const SECTION_KEYS = {
+  blocklist: 'blacklist',
+  fuzzylist: 'fuzzylist',
+  allowlist: 'whitelist',
+};
+
+const addHosts = (section, domains, dest) => {
+  const cfg = {
+    ...config,
+    [section]: config[section].concat(domains),
+  };
+
+  const output = JSON.stringify(cfg, null, 2) + '\n';
+
+  fs.writeFile(dest, output, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+  });
+}
+
+const validateHostRedundancy = (detector, section, h) => {
+  switch (section) {
+    case 'blocklist': {
+      const r = detector.check(h);
+      if (r.result) {
+        throw new Error(`'${h}' already covered by '${r.match}' in '${r.type}'.`);
+      }
+      return true;
+    }
+    case 'allowlist': {
+      const r = detector.check(h);
+      if (!r.result) {
+        console.error(`'${h}' does not require allowlisting`);
+        return false;
+      }
+      return true;
+    }
+    case 'fuzzylist': {
+      if (config.fuzzylist.includes(h)) {
+        console.error(`'${h}' already in fuzzylist`);
+        return false;
+      }
+      return true;
+    }
+    default:
+      throw new Error(`unrecognized section '${section}'`);
+  }
+}
+
+module.exports = {
+  addHosts,
+  validateHostRedundancy,
+};
+
+/////////////////////
+//////// MAIN ///////
+/////////////////////
+
+const exitWithUsage = (exitCode) => {
+  console.error(`Usage: ${
+    process.argv.slice(0,2).join(' ')
+  } src/config.json ${
+    Object.keys(SECTION_KEYS).join('|')
+  } hostname...`);
+  process.exit(exitCode);
+};
+
+if (require.main === module) {
+  if (process.argv.length < 4) {
+    exitWithUsage(1);
+  }
+
+  const [destFile, section, ...hosts] = process.argv.slice(2);
+
+  if (!Object.keys(SECTION_KEYS).includes(section) || hosts.length < 1) {
+    exitWithUsage(1);
+  }
+
+  const detector = new PhishingDetector(config);
+  let newHosts = [];
+
+  try {
+    newHosts = hosts.filter(h => validateHostRedundancy(detector, section, h));
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  addHosts(SECTION_KEYS[section], newHosts, destFile);
+}
--- src/detector.js
+++ src/detector.js
@@ -79,15 +79,21 @@ class PhishingDetector {
     const source = domainToParts(fqdn)
 
     for (const { allowlist, name, version } of this.configs) {
-      // if source matches whitelist domain (or subdomain thereof), PASS
-      const whitelistMatch = matchPartsAgainstList(source, allowlist)
-      if (whitelistMatch) return { name, result: false, type: 'allowlist', version }
+      // if source matches allowlist hostname (or subdomain thereof), PASS
+      const allowlistMatch = matchPartsAgainstList(source, allowlist)
+      if (allowlistMatch) {
+        const match = domainPartsToDomain(allowlistMatch);
+        return { match, name, result: false, type: 'allowlist', version }
+      }
     }
 
     for (const { blocklist, fuzzylist, name, tolerance, version } of this.configs) {
-      // if source matches blacklist domain (or subdomain thereof), FAIL
-      const blacklistMatch = matchPartsAgainstList(source, blocklist)
-      if (blacklistMatch) return { name, result: true, type: 'blocklist', version }
+      // if source matches blocklist hostname (or subdomain thereof), FAIL
+      const blocklistMatch = matchPartsAgainstList(source, blocklist)
+      if (blocklistMatch) {
+        const match = domainPartsToDomain(blocklistMatch);
+        return { match, name, result: true, type: 'blocklist', version }
+      }
 
       if (tolerance > 0) {
         // check if near-match of whitelist domain, FAIL
@@ -175,11 +181,12 @@ function domainPartsToFuzzyForm(domainParts) {
 }
 
 // match the target parts, ignoring extra subdomains on source
+// returns parts for first found matching entry
 //   source: [io, metamask, xyz]
 //   target: [io, metamask]
 //   result: PASS
 function matchPartsAgainstList(source, list) {
-  return list.some((target) => {
+  return list.find((target) => {
     // target domain has more parts than source, fail
     if (target.length > source.length) return false
     // source matches target or (is deeper subdomain)
