39a3972b8c3e24498afc3a0105c72de333400e2f
legobt <6wbvkn0j@anonaddy.me>
2023-03-27 11:30:59 +0000

    wip: ci: add add-hosts workflow

---
+++ .github/workflows/process-hostlists.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# process_list adds new hosts from ADD_HOSTS_HERE subdirectory to list
+process_list() {
+  _list=$1
+  git show ADD_HOSTS_HERE/$_list | grep . >/dev/null \
+    && node src/add-hosts.js $_list \
+      $(git show --name-only --oneline ADD_HOSTS_HERE/$_list/ | tail -n +2 | xargs -r grep -hEv '^$|^#') \
+    && echo "done $_list" || echo "skipped $_list"
+}
+
+process_list allowlist
+process_list blocklist
+process_list fuzzylist
+
+# name of new branch
+export _branch=ci-merge-$(git rev-parse --short HEAD)
+
+# create commit with list changes and push
+git diff src/config.json \
+    && git checkout -b $_branch \
+    && git add src/config.json \
+    && git rm ADD_HOSTS_HERE/*/* \
+    && git commit -m 'update hostlists' \
+    && git push -u origin $_branch
+echo 'done'
