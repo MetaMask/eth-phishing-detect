aae89bf8ef21fcd60d36f51b1bbe6cdaebd1ab87
legobeat <109787230+legobeat@users.noreply.github.com>
2023-03-29 02:01:53 +0900

    [chore] update devDependencies (#12142)
    * devDeps: async@2.6.4->3.2.4
    
    * devDeps: csv-parse@4.4.6->5.3.6
    
    * devDeps: needle@2.2.4->3.2.0
    
    * devDeps: punycode@2.1.1->2.3.0
    
    * devDeps: tape@4.9.1->5.6.3
    
    * devDeps/resolutions: browserify>assert@1.5.0->2.0.0
    
    avoid pulling in object-assign subdependency
    
    * devDeps: bump lockfile
    
    `yarn upgrade`: upgrade while keeping version constraints
    
    ---------
    
    Co-authored-by: AlexHerman1 <38788573+AlexHerman1@users.noreply.github.com>

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ const fs = require("fs")
 const test = require("tape")
 const needle = require('needle')
 const mapValues = require('async/mapValues')
-const parseCsv = require("csv-parse/lib/sync")
+const parseCsv = require('csv-parse/sync')
 const punycode = require('punycode/')
 const PhishingDetector = require('../src/detector.js')
 const { cleanAllowlist, cleanBlocklist } = require('../src/clean-config.js')
@@ -32,7 +32,7 @@ function loadMetamaskGaq () {
   // extract hits from Google Analytics data from metamask.io phishing warning
   // fetch from https://analytics.google.com/analytics/web/#my-reports/N6OapMZATf-zAzHjpa9Wcw/a37075177w102798190p106879314/%3F_u.dateOption%3Dlast7days%26454-table.plotKeys%3D%5B%5D%26454-table.rowStart%3D0%26454-table.rowCount%3D250/
   const rawCsv = fs.readFileSync(__dirname + '/metamaskGaq.csv', 'utf8')
-  const result = parseCsv(rawCsv, {
+  const result = parseCsv.parse(rawCsv, {
     skip_empty_lines: true,
     comment: '#',
     columns: true,
