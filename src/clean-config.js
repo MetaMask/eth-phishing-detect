7db47686fdff62051f73983815b9848d8e8e4b65
legobeat <109787230+legobeat@users.noreply.github.com>
2023-03-29 01:39:27 +0900

    test: fail if blocklist or allowlist contain redundant entries (#12138)
    * remove redundant allowlist entries
    
    * test: ensure ethereum.org is not blocked regardless of presence in allowlist
    
    * scripts/clean-config: export cleanAllowlist/cleanBlocklist functions
    
    * test: ensure blocklist and allowlist contain no redundant entries
    
    * remove redundant blocklist entry
    
    ---------
    
    Co-authored-by: AlexHerman1 <38788573+AlexHerman1@users.noreply.github.com>

--- src/clean-config.js
+++ src/clean-config.js
@@ -2,72 +2,106 @@
 const { writeFileSync } = require('fs');
 const path = require('path');
 const PhishingDetector = require('./detector');
-const config = require('./config.json');
 
 const SECTION_KEYS = {
   blocklist: 'blacklist',
   allowlist: 'whitelist',
 };
 
-const exitWithUsage = (exitCode) => {
-  console.error(`Usage: ${
-    process.argv.slice(0,2).join(' ')
-  } ${
-    Object.keys(SECTION_KEYS).join('|')
-  }`);
-  console.error('Removes redundant entries from config section and writes filtered config to standard output');
-  process.exit(exitCode);
-};
+const cleanConfig = (config, listName) => {
+  const section = SECTION_KEYS[listName];
+  const newConfig = {
+    version: config.version,
+    tolerance: listName === 'blockList' ? 0 : config.tolerance, // disable fuzzychecking for performance
+    fuzzylist: [...config.fuzzylist],
+    [SECTION_KEYS.allowlist] : [...config[SECTION_KEYS.allowlist]],
+    [SECTION_KEYS.blocklist] : [...config[SECTION_KEYS.blocklist]],
+  };
 
-const listName = process.argv[2];
-const section = SECTION_KEYS[listName];
-if (!section) {
-  exitWithUsage(1);
-}
+  const finalEntries = new Set();
+  const detector = new PhishingDetector(newConfig);
+  const baseList = detector.configs[0][listName];
 
-const cleanConfig = {
-  version: config.version,
-  tolerance: listName === 'blockList' ? 0 : config.tolerance, // disable fuzzychecking for performance
-  fuzzylist: [...config.fuzzylist],
-  [SECTION_KEYS.allowlist] : [...config[SECTION_KEYS.allowlist]],
-  [SECTION_KEYS.blocklist] : [...config[SECTION_KEYS.blocklist]],
-};
+  const isResultRedundant = listName === 'blocklist'
+    ? r => r.result && r.type !== 'fuzzy'
+    : r => !r.result;
 
-const finalEntries = new Set();
-const detector = new PhishingDetector(cleanConfig);
-const baseList = detector.configs[0][listName];
+  for (let i = 0; i < config[section].length; i++) {
+    const host = config[section][i];
 
-const isResultRedundant = listName === 'blocklist'
-  ? r => r.result && r.type !== 'fuzzy'
-  : r => !r.result;
+    // omit current domain from current list to see if results differ without
+    detector.configs[0][listName] = baseList.slice(0,i).concat(baseList.slice(i+1));
+    const result = detector.check(host);
 
-for (let i = 0; i < config[section].length; i++) {
-  const host = config[section][i];
-
-  // omit current domain from current list to see if results differ without
-  detector.configs[0][listName] = baseList.slice(0,i).concat(baseList.slice(i+1));
-  const result = detector.check(host);
-
-  if (!isResultRedundant(result)) {
-    finalEntries.add(host);
-  } else {
-    if (listName === 'allowlist') {
-      console.error(`removing redundant ${JSON.stringify({entry: host, result: result.result, tolerance: config.tolerance})}`);
+    if (!isResultRedundant(result)) {
+      finalEntries.add(host);
     } else {
-      console.error(`removing redundant ${JSON.stringify({entry: host, match: result.match, matchList: result.type})}`);
+      if (listName === 'allowlist') {
+        console.error(`removing redundant ${JSON.stringify({entry: host, result: result.result, tolerance: config.tolerance})}`);
+      } else {
+        console.error(`removing redundant ${JSON.stringify({entry: host, match: result.match, matchList: result.type})}`);
+      }
     }
   }
-}
 
-cleanConfig[section] = Array.from(finalEntries);
-cleanConfig.tolerance = config.tolerance;
+  newConfig[section] = Array.from(finalEntries);
+  newConfig.tolerance = config.tolerance;
+  return newConfig;
+};
 
-// if this is piped to another process, then write to stdout.
-// otherwise, write new config to disk.
-const result = JSON.stringify(cleanConfig, undefined, 2)+'\n';
-if (process.stdout.isTTY) {
-  const destinationPath = process.env['MM_OUTPUT_PATH'] || './config.json';
-  writeFileSync(path.join(__dirname, destinationPath), result);
-} else {
-  process.stdout.write(result);
+const cleanAllowlist = config =>
+  cleanConfig(config, 'allowlist');
+
+const cleanBlocklist = config =>
+  cleanConfig(config, 'blocklist');
+
+module.exports = {
+  cleanAllowlist,
+  cleanBlocklist,
+};
+
+/////////////////////
+//////// MAIN ///////
+/////////////////////
+
+if (require.main === module) {
+  const exitWithUsage = (exitCode) => {
+    console.error(`Usage: ${
+      process.argv.slice(0,2).join(' ')
+    } ${
+      Object.keys(SECTION_KEYS).join('|')
+    }`);
+    console.error('Removes redundant entries from config section and writes filtered config to standard output');
+    process.exit(exitCode);
+  };
+
+  const listName = process.argv[2];
+  const section = SECTION_KEYS[listName];
+
+  if (!section) {
+    exitWithUsage(1);
+  }
+
+  try {
+    const config = require('./config.json')
+    const newConfig = cleanConfig(config, listName);
+
+    // if this is piped to another process, then write to stdout.
+    // otherwise, write new config to disk.
+    const result = JSON.stringify(newConfig, undefined, 2)+'\n';
+    if (process.stdout.isTTY) {
+      const destinationPath = process.env['MM_OUTPUT_PATH'] || './config.json';
+      writeFileSync(path.join(__dirname, destinationPath), result);
+    } else {
+      process.stdout.write(result);
+    }
+
+    if (newConfig[section].length !== config[section].length) {
+      console.error(JSON.stringify({newCount: newConfig[section].length, oldCount: config[section].length}));
+      process.exit(2);
+    }
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 }
