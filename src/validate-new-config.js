3f20285e887bff4a35f2fb777fbe4f7335de3eb6
legobeat <109787230+legobeat@users.noreply.github.com>
2023-04-26 02:12:43 +0000

    add git pre-commit hook and ci test validating list invariants (#12164)
    * devDeps: add simple-git-hooks
      - new lifecycle script: init simple-git-hooks on yarn prepare
    
    * Add validate-new-config.js
      - Diffs a base config with incoming. Exit with non-zero if invariants are not met.
    
    * githook: add pre-commit validating list invariants on list changes
      - parameterize git pre-commit hook with base/target refs (default: HEAD / staged)
    
    * scripts/validate-new-config: Perform duplicate check
    * scripts/validate-new-config: Perform internal redundancy check
    * validate-new-config in ci

---
+++ src/validate-new-config.js
@@ -0,0 +1,82 @@
+#!/usr/bin/env node
+
+const { readFileSync } = require('fs');
+const PhishingDetector = require('../src/detector.js');
+const { validateHostRedundancy } = require('../src/add-hosts.js');
+
+const SECTION_KEYS = {
+  blocklist: 'blacklist',
+  allowlist: 'whitelist',
+};
+
+const exitWithUsage = (exitCode) => {
+  console.error(`Usage: ${
+    process.argv.slice(0,2).join(' ')
+  } baseconfig.json newconfig.json`);
+  console.error('Compares diff between base and new config, validating invariants');
+  process.exit(exitCode);
+};
+
+const exitWithFail = (message) => {
+  console.error(`[LIST ERROR]: ${message}`);
+  process.exit(1);
+};
+
+if (process.argv.length !== 4) {
+  exitWithUsage(1);
+}
+
+try {
+  const [baseConfig, newConfig] = process.argv.slice(2).map(p => JSON.parse(readFileSync(p)));
+  {
+    // 1. Fuzzylist is remove-only
+    let result;
+    if (result = newConfig.fuzzylist.find(h => !baseConfig.fuzzylist.includes(h))) {
+      exitWithFail(`unexpected fuzzylist entry "${result}"`);
+    }
+  }
+  {
+    // 2. Fuzzy-tolerance is strictly bounded to not grow
+    if (!(newConfig.tolerance <= baseConfig.tolerance)) {
+      exitWithFail(`new tolerance ${newConfig.tolerance} must be <= old tolerance ${oldConfig.tolerance}`);
+    }
+  }
+  {
+    // 3. New entries are not redundant
+    ['blocklist','allowlist'].forEach(listName => {
+      const section = SECTION_KEYS[listName];
+      const baseHosts = new Set(baseConfig[section]);
+      const newHosts = new Set(newConfig[section].filter(h => !baseHosts.has(h)));
+      if (new Set(newConfig[section]).size < newConfig[section].length) {
+        return exitWithFail(`${listName} contains duplicate entry`);
+      }
+
+      // entry-wise checking is typically faster than doing a full consistency check
+      const checkList = [...baseConfig[section]];
+      for (const host of newHosts) {
+        const cfg = {
+          ...baseConfig,
+          [section]: checkList,
+        };
+        const detector = new PhishingDetector(cfg);
+        if (!validateHostRedundancy(detector, listName, host)) {
+          return exitWithFail(`${listName} "${host}" failed redundancy check`);
+        }
+        checkList.push(host);
+      }
+    });
+  }
+  // TODO: enable once lists are sorted in `config.json`
+  /*
+  {
+    // 4. Entries are sorted alphabetically
+    ['fuzzylist','blacklist','whitelist'].forEach(listName => {
+      if(!deepEqual(newConfig[listName], [...newConfig[listName]].sort())) {
+        exitWithFail(`${listName} not sorted`);
+      }
+    });
+  }
+  */
+} catch (err) {
+  exitWithFail(err.message || err);
+}
