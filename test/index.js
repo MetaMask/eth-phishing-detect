1fbece8e5cf2a1c3dd3e6aee395b03f0daba5ee2
menox <mmenoxi@gmail.com>
2023-05-15 16:13:06 +0100

    feat(): added confirm blocklist test

--- test/index.js
+++ test/index.js
@@ -1,27 +1,28 @@
 #!/usr/bin/env node
 
-const { runTests: runDetectorTests } = require('./detector.test.js');
-const { runTests: runConfigTests } = require('./config.test.js');
+const { runTests: runDetectorTests } = require("./detector.test.js");
+const { runTests: runConfigTests } = require("./config.test.js");
+const {
+  runTests: runConfirmBlocklistTests,
+} = require("./confirm-blocklist.test.js");
 
-const config = require('../src/config.json');
+const config = require("../src/config.json");
 
 const SUITES = {
-  config: [
-    () => runConfigTests({config}),
-  ],
-  unit: [
-    runDetectorTests,
-  ],
-}
+  config: [() => runConfigTests({ config })],
+  unit: [runDetectorTests],
+  confirmBlocklist: [() => runConfirmBlocklistTests(config)],
+};
 
-function runTests (target = 'all') {
-  const suites = target === 'all'
-    ? [...Object.values(SUITES).flatMap(ss => [...ss])]
-    : SUITES[target];
+function runTests(target = "all") {
+  const suites =
+    target === "all"
+      ? [...Object.values(SUITES).flatMap((ss) => [...ss])]
+      : SUITES[target];
   for (const suite of suites) {
     suite();
   }
 }
 
-const target = process.argv[2] || 'all';
+const target = process.argv[2] || "all";
 runTests(target);
