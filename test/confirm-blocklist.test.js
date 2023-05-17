5a92b30956533b8113860a2fd048e3498e374db2
menox <mmenoxi@gmail.com>
2023-05-17 10:40:43 +0100

    feat(): added confirm blocklist tranco test

--- test/confirm-blocklist.test.js
+++ test/confirm-blocklist.test.js
@@ -5,7 +5,7 @@ const parseCsv = require("csv-parse/sync");
 const fs = require("fs");
 
 function runTests(config) {
-  test("check config blocklist against whitelist", (t) => {
+  test.skip("check config blocklist against alexa top domains", (t) => {
     const alexaDomainsCsv = fs.readFileSync(
       __dirname + "/alexaDomains.csv",
       "utf8"
@@ -38,10 +38,74 @@ function runTests(config) {
       t.notEqual(
         value.type,
         "allowlist",
-        `result: "${domain}" not found in: "allowlist"`
+        `result: "${domain}" should not be in: "allowlist" at: ${value.match}`
       );
     });
 
+    t.end();
+  });
+
+  test("check config blocklist against top Tranco domains", async (t) => {
+    const trancoDomainsCsv = fs.readFileSync(
+      __dirname + "/trancoTop1m.csv",
+      "utf8"
+    );
+    const trancoDomains = parseCsv
+      .parse(trancoDomainsCsv, {
+        skip_empty_lines: true,
+      })
+      .flat();
+
+    const options = [
+      {
+        allowlist: trancoDomains,
+        blocklist: [],
+        fuzzylist: [],
+        name: "first",
+        tolerance: 0,
+        version: 1,
+      },
+    ];
+
+    const detector = new PhishingDetector(options);
+
+    config.blacklist.forEach((domain) => {
+      // First method
+      /* const value = trancoDomains.includes(domain);
+        t.equal(value, false, `result: "${domain}" not found in: "allowlist"`); */
+      // Second method
+      const value = detector.check(domain);
+      t.notEqual(
+        value.type,
+        "allowlist",
+        `result: "${domain}" should not be in "allowlist" at: ${value.match}`
+      );
+    });
+
+    t.end();
+  });
+
+  test.skip("check config blocklist against top DappRadar dapps", async (t) => {
+    const query = new URLSearchParams({
+      chain: "string",
+      category: "0",
+      range: "24h",
+      top: "10",
+    }).toString();
+
+    const metric = "YOUR_metric_PARAMETER";
+    const project = "4tsxo4vuhotaojtl";
+    const resp = await fetch(
+      `https://api.dappradar.com/${project}/dapps/top/${metric}?${query}`,
+      {
+        method: "GET",
+        headers: {
+          "X-BLOBR-KEY": "YOUR_API_KEY_HERE",
+        },
+      }
+    );
+
+    const data = await resp.text();
     t.end();
   });
 }
