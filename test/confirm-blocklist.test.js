5732fb02a83d711db610b134ad72431e42654c16
menox <mmenoxi@gmail.com>
2023-06-08 10:26:56 +0100

    feat(): added coinmarketcap api list update

--- test/confirm-blocklist.test.js
+++ test/confirm-blocklist.test.js
@@ -1,14 +1,15 @@
 const test = require("tape");
 const { testDomain } = require("./test.util.js");
 const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("./tranco.db");
+const trancoDb = new sqlite3.Database("./tranco.db");
+const coinmarketcapDb = new sqlite3.Database("./coinmarketcap.db");
 
 function runTests(config) {
-  test("check config blocklist against top Tranco domains", async (t) => {
+  test("check config blocklist against Tranco domains", async (t) => {
     let foundWhitelistedDomains = [];
-    db.parallelize(() => {
+    trancoDb.parallelize(() => {
       config.blacklist.forEach((BlacklistDomain, index) => {
-        db.get(
+        trancoDb.get(
           "SELECT domain FROM tranco WHERE domain = ?",
           BlacklistDomain,
           function (err, row) {
@@ -20,10 +21,35 @@ function runTests(config) {
       });
     });
 
-    db.close(function (err) {
+    trancoDb.close(function (err) {
       if (foundWhitelistedDomains.length > 0) {
         t.fail(
           `Following domains found in tranco whitelist: "${foundWhitelistedDomains}" `
+        );
+      }
+    });
+  });
+
+  test("check config blocklist against Coinmarketcap coins domains", async (t) => {
+    let foundWhitelistedDomains = [];
+    coinmarketcapDb.parallelize(() => {
+      config.blacklist.forEach((BlacklistDomain, index) => {
+        coinmarketcapDb.get(
+          "SELECT domain FROM coinmarketcap WHERE domain = ?",
+          BlacklistDomain,
+          function (err, row) {
+            if (row) {
+              foundWhitelistedDomains.push(row.domain);
+            }
+          }
+        );
+      });
+    });
+
+    coinmarketcapDb.close(function (err) {
+      if (foundWhitelistedDomains.length > 0) {
+        t.fail(
+          `Following domains found in Coinmarketcap coins domains: "${foundWhitelistedDomains}" `
         );
       }
     });
--- test/update-static-lists.js
+++ test/update-static-lists.js
@@ -78,4 +78,217 @@ async function updateTrancoList() {
   });
 }
 
-updateTrancoList();
+async function updateDappradarList() {
+  try {
+    fs.unlinkSync("./dappradar-temp.db");
+  } catch (err) {
+    console.error(err);
+  }
+
+  const projectId = "";
+  const apiKey = "";
+
+  // Create db and fill it with entries from Dappradar API responses
+  const db = new sqlite3.Database("./dappradar-temp.db");
+  let dappDomains = [];
+
+  function delay(t) {
+    return new Promise((resolve) => setTimeout(resolve, t));
+  }
+
+  // Call Dappradar API
+  try {
+    const response = await axios.get(
+      `https://api.dappradar.com/${projectId}/dapps?resultsPerPage=50`,
+      {
+        headers: {
+          "X-BLOBR-KEY": apiKey,
+        },
+      }
+    );
+
+    for (const dappItem of response.data.results) {
+      try {
+        await delay(500);
+        const dapp = await axios.get(
+          `https://api.dappradar.com/${projectId}/dapps/${dappItem.dappId}`,
+          {
+            headers: {
+              "X-BLOBR-KEY": apiKey,
+            },
+          }
+        );
+        dappDomains.push([dapp.name, dapp.website]);
+      } catch (error) {
+        console.error(
+          `Problems while calling Dappradar API dapp endpoint for dapp: ${dappItem.dappId}. Error: ${error}`
+        );
+      }
+    }
+  } catch (error) {
+    console.error(
+      "Problems while calling Dappradar API dapps listing endpoint. Error: " +
+        error
+    );
+    process.exit(1);
+  }
+
+  db.on("trace", function (sqlString) {
+    console.log("SQL string: " + sqlString);
+  });
+
+  db.serialize(() => {
+    db.exec("CREATE TABLE dappradar (name TEXT, website TEXT)");
+    db.exec("CREATE INDEX website_index ON dappradar(website);");
+
+    // Wrapping all insert statements in a single transaction is way faster as
+    // sqlite by default insert each row in a separate transaction
+    db.exec("BEGIN TRANSACTION;");
+
+    const stmt = db.prepare("INSERT INTO dappradar VALUES (?,?)");
+
+    for (dapp of dappDomains) {
+      const dappDomainSplit1 = dapp[1].split(/(https:\/\/|http:\/\/)+/);
+      const dappDomainSplit2 = dappDomainSplit1[2].split(/(\/)+/);
+      const dappDomainName = dappDomainSplit2[0].replace("www.", "");
+      stmt.run(dapp[0], dappDomainName);
+    }
+
+    stmt.finalize();
+
+    db.exec("COMMIT;");
+  });
+
+  db.close(function (err) {
+    if (err) {
+      process.exit(1);
+    } else {
+      // copy temp db file
+      console.log("Copying: temp db file... ");
+      fs.copyFileSync("./dappradar-temp.db", "./dappradar.db");
+      process.exit(0);
+    }
+  });
+}
+
+async function updateCoinmarketcapList() {
+  try {
+    fs.unlinkSync("./coinmarketcap-temp.db");
+  } catch (err) {
+    console.error(err);
+  }
+
+  const apiKey = "";
+  let coinsIds = [];
+  const coinsArray = [];
+
+  // Create db and fill it with entries from Dappradar API responses
+  const db = new sqlite3.Database("./coinmarketcap-temp.db");
+
+  function delay(t) {
+    return new Promise((resolve) => setTimeout(resolve, t));
+  }
+
+  // Call Coinmarketcap API
+  try {
+    const response = await axios.get(
+      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=100",
+      {
+        headers: {
+          "X-CMC_PRO_API_KEY": apiKey,
+        },
+      }
+    );
+    response.data.data.forEach((coin) => {
+      coinsIds.push(coin.id);
+    });
+
+    try {
+      await delay(2500);
+      const response = await axios.get(
+        `https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?id=${coinsIds.join(
+          ","
+        )}`,
+        {
+          headers: {
+            "X-CMC_PRO_API_KEY": apiKey,
+          },
+        }
+      );
+
+      Object.keys(response.data.data).forEach((coinId) => {
+        coinsArray.push([
+          response.data.data[coinId].name,
+          response.data.data[coinId].urls.website[0],
+        ]);
+      });
+    } catch (error) {
+      console.error(
+        `Problems while calling Coinmarketcap API coins details endpoint. Error: ${error}`
+      );
+      process.exit(1);
+    }
+  } catch (error) {
+    console.error(
+      "Problems while calling Coinmarketcap API coins listing endpoint. Error: " +
+        error
+    );
+    process.exit(1);
+  }
+
+  db.on("trace", function (sqlString) {
+    console.log("SQL string: " + sqlString);
+  });
+
+  db.serialize(() => {
+    db.exec("CREATE TABLE coinmarketcap (name TEXT, domain TEXT)");
+    db.exec("CREATE INDEX domain_index ON coinmarketcap(domain);");
+
+    // Wrapping all insert statements in a single transaction is way faster as
+    // sqlite by default insert each row in a separate transaction
+    db.exec("BEGIN TRANSACTION;");
+
+    const stmt = db.prepare("INSERT INTO coinmarketcap VALUES (?,?)");
+
+    for (coin of coinsArray) {
+      const coinDomainSplit1 = coin[1].split(/(https:\/\/|http:\/\/)+/);
+      const coinDomainSplit2 = coinDomainSplit1[2].split(/(\/)+/);
+      const coinDomainName = coinDomainSplit2[0].replace("www.", "");
+      stmt.run(coin[0], coinDomainName);
+    }
+
+    stmt.finalize();
+
+    db.exec("COMMIT;");
+  });
+
+  db.close(function (err) {
+    if (err) {
+      process.exit(1);
+    } else {
+      // copy temp db file
+      console.log("Copying: temp db file... ");
+      fs.copyFileSync("./coinmarketcap-temp.db", "./coinmarketcap.db");
+      process.exit(0);
+    }
+  });
+}
+
+const target = process.argv[2];
+
+switch (target) {
+  case "tranco":
+    updateTrancoList();
+    break;
+  case "coinmarketcap":
+    updateCoinmarketcapList();
+    break;
+  case "dappradar":
+    updateDappradarList();
+    break;
+  default:
+    console.log(
+      'You need to specify either: "tranco" | "coinmarketcap" | "dappradar"'
+    );
+    break;
+}
