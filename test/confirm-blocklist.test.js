e85a83365fa5f12180ffd60f8f42cdfbd1411ff8
legobt <6wbvkn0j@anonaddy.me>
2023-08-02 19:13:35 +0000

    for science: move list loading inside test units to include entire runtime cost

--- test/confirm-blocklist.test.js
+++ test/confirm-blocklist.test.js
@@ -3,18 +3,18 @@ const fs = require("fs");
 const path = require("path");
 
 const DB_PATH = path.join(__dirname, "db");
-const trancos = new Set(fs.readFileSync(path.join(DB_PATH, "trancos"), {encoding: 'utf-8'}).split('\n'));
-const coinmarketcaps = new Set(fs.readFileSync(path.join(DB_PATH, "coinmarketcaps"), {encoding: 'utf-8'}).split('\n'));
 
 
 function runTests(config) {
   test("check config blocklist against Tranco domains", (t) => {
+    const trancos = new Set(fs.readFileSync(path.join(DB_PATH, "trancos"), {encoding: 'utf-8'}).split('\n'));
     const foundOverlapping = config.blacklist.filter(d => trancos.has(d));
     t.equal(foundOverlapping.length, 0, `Following domains found in Tranco domains: "${foundOverlapping}"`);
     t.end();
   });
 
   test("check config blocklist against Coinmarketcap coins domains", (t) => {
+    const coinmarketcaps = new Set(fs.readFileSync(path.join(DB_PATH, "coinmarketcaps"), {encoding: 'utf-8'}).split('\n'));
     const foundOverlapping = config.blacklist.filter(d => coinmarketcaps.has(d));
     t.equal(foundOverlapping.length, 0, `Following domains found in Coinmarketcap coins domains: "${foundOverlapping}"`);
     t.end();
