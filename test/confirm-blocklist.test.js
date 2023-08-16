28fe494feb974d62b60da1b0b57e5935c4b28374
legobeat <109787230+legobeat@users.noreply.github.com>
2023-08-16 08:48:19 +0000

    replace sqlite3 with text files (#13188)
    * WIP: replace sqlite3 with text files
    
    * for science: move list loading inside test units to include entire runtime cost

--- test/confirm-blocklist.test.js
+++ test/confirm-blocklist.test.js
@@ -1,62 +1,23 @@
 const test = require("tape");
-const { testDomain } = require("./test.util.js");
-const join = require("path").join;
-const sqlite3 = require("sqlite3").verbose();
+const fs = require("fs");
+const path = require("path");
 
-const DB_PATH = join(__dirname) + "/db";
-const trancoDb = new sqlite3.Database(DB_PATH + "/tranco.db");
-const coinmarketcapDb = new sqlite3.Database(DB_PATH + "/coinmarketcap.db");
+const DB_PATH = path.join(__dirname, "db");
 
 
 function runTests(config) {
-  test("check config blocklist against Tranco domains", async (t) => {
-    let foundWhitelistedDomains = [];
-    trancoDb.parallelize(() => {
-      config.blacklist.forEach((BlacklistDomain, index) => {
-        trancoDb.get(
-          "SELECT domain FROM tranco WHERE domain = ?",
-          BlacklistDomain,
-          function (err, row) {
-            if (row) {
-              foundWhitelistedDomains.push(row.domain);
-            }
-          }
-        );
-      });
-    });
-
-    trancoDb.close(function (err) {
-      if (foundWhitelistedDomains.length > 0) {
-        t.fail(
-          `Following domains found in tranco whitelist: "${foundWhitelistedDomains}" `
-        );
-      }
-    });
+  test("check config blocklist against Tranco domains", (t) => {
+    const trancos = new Set(fs.readFileSync(path.join(DB_PATH, "trancos"), {encoding: 'utf-8'}).split('\n'));
+    const foundOverlapping = config.blacklist.filter(d => trancos.has(d));
+    t.equal(foundOverlapping.length, 0, `Following domains found in Tranco domains: "${foundOverlapping}"`);
+    t.end();
   });
 
-  test("check config blocklist against Coinmarketcap coins domains", async (t) => {
-    let foundWhitelistedDomains = [];
-    coinmarketcapDb.parallelize(() => {
-      config.blacklist.forEach((BlacklistDomain, index) => {
-        coinmarketcapDb.get(
-          "SELECT domain FROM coinmarketcap WHERE domain = ?",
-          BlacklistDomain,
-          function (err, row) {
-            if (row) {
-              foundWhitelistedDomains.push(row.domain);
-            }
-          }
-        );
-      });
-    });
-
-    coinmarketcapDb.close(function (err) {
-      if (foundWhitelistedDomains.length > 0) {
-        t.fail(
-          `Following domains found in Coinmarketcap coins domains: "${foundWhitelistedDomains}" `
-        );
-      }
-    });
+  test("check config blocklist against Coinmarketcap coins domains", (t) => {
+    const coinmarketcaps = new Set(fs.readFileSync(path.join(DB_PATH, "coinmarketcaps"), {encoding: 'utf-8'}).split('\n'));
+    const foundOverlapping = config.blacklist.filter(d => coinmarketcaps.has(d));
+    t.equal(foundOverlapping.length, 0, `Following domains found in Coinmarketcap coins domains: "${foundOverlapping}"`);
+    t.end();
   });
 }
 
