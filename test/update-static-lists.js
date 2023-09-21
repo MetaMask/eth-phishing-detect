6de51202b4bc7157255bb57190b1984138727de3
Menox <marcomenozzi80@gmail.com>
2023-09-21 21:50:18 +0200

    Use third party lists to reduce false positives in blocklist (#13588)
    * feat(): added confirm blocklist test
    
    * feat(): added confirm blocklist tranco test
    
    * feat(): added tranco db
    
    * feat(): vscode dir removed
    
    * chore(): found domains changed from string to array
    
    * feat(): added coinmarketcap api list update
    
    * feat(): added entries in gitignore
    
    * feat(): added keys as env variables. Tranco list fine tuned
    
    * chore(): axios lib removed and needle used instead
    
    * chore(): axios package removed
    
    * chore(): sqlite and dotenc moved to dev deps
    
    * Added logic to create file before r/w error thrown
    
    * Add update commands
    
    * Fixed bug and ignore file
    
    * Modified CMC db update flow to use common env file, added built dbs
    
    * Removed cache file
    
    * Removed cache file
    
    * Updated README to clarify what the databases are for
    
    * Moved strings to dict
    
    * replace sqlite3 with text files (#13188)
    
    * WIP: replace sqlite3 with text files
    
    * for science: move list loading inside test units to include entire runtime cost
    
    * feat(): update lists script updated for text lists
    
    * chore(): some refactoring
    
    * Added file extension
    
    * Added file extensions
    
    * remove myraidbox.de
    
    * Mirror same logic as tranco to include exclude list
    
    ---------
    
    Co-authored-by: HARRY DENLEY <HARRY.DENLEY@OUTLOOK.COM>
    Co-authored-by: legobeat <109787230+legobeat@users.noreply.github.com>
    Co-authored-by: Harry <409H@users.noreply.github.com>

---
+++ test/update-static-lists.js
@@ -0,0 +1,169 @@
+const fs = require("fs");
+const needle = require("needle");
+const join = require("path").join;
+require("dotenv").config({ path: join(__dirname, "/.update-lists.env") });
+
+const DB_PATH = join(__dirname) + "/db";
+
+const ENDPOINTS = {
+  TRANCO_LIST: "https://tranco-list.eu/download/K25GW/100000",
+  COINMARKETCAP:
+    "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=5000&start=1&sort=market_cap_strict&market_cap_min=10000000",
+  COINMARKETCAP_COIN_INFO:
+    "https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?id=",
+};
+
+function arrayTo2DArray1(arr, size) {
+  const res = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i % size === 0) {
+      // Push a new array containing the current value to the res array
+      res.push([arr[i]]);
+    } else {
+      // Push the current value to the current array
+      res[res.length - 1].push(arr[i]);
+    }
+  }
+  return res;
+}
+
+const touchFile = (path) => {
+  const time = new Date();
+  try {
+    fs.utimesSync(path, time, time);
+  } catch (err) {
+    fs.closeSync(fs.openSync(path, "w"));
+  }
+};
+
+async function updateTrancoList() {
+  const PATH_CSV = DB_PATH + "/trancoList.csv";
+
+  // Download updated list
+  const stream = fs.createWriteStream(PATH_CSV);
+
+  try {
+    needle.get(ENDPOINTS.TRANCO_LIST).pipe(stream);
+
+    await new Promise((resolve, reject) => {
+      stream.on("finish", resolve);
+      stream.on("error", reject);
+    });
+  } catch (error) {
+    console.error(
+      "Problems while downloading the latest list. Error: " + error
+    );
+    process.exit(1);
+  }
+
+  await touchFile(PATH_CSV);
+
+  const trancoDomainsCsv = fs.readFileSync(PATH_CSV, "utf8");
+  // Replace everything before a comma with empty string and convert line feeds to ln
+  let trancoDomains = trancoDomainsCsv.split(/.*,/).join("");
+  trancoDomains = trancoDomains.split(/\r\n/).join("\n");
+
+  try {
+    fs.writeFileSync(DB_PATH + "/trancos.txt", trancoDomains);
+    process.exit(0);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+async function updateCoinmarketcapList() {
+  const apiKey = process.env.COINMARKETCAP_API_KEY;
+  let coinsIds = [];
+  let coinsMarketCaps = {};
+  const coinsArray = [];
+  const coinsPerSubCall = 250; // how many coins (IDs) will be in the query string of coins metadata subcalls
+
+  function delay(t) {
+    return new Promise((resolve) => setTimeout(resolve, t));
+  }
+
+  // Call Coinmarketcap API
+  try {
+    // get only coins with market cap > 10M
+    const response = await needle("get", ENDPOINTS.COINMARKETCAP, {
+      headers: {
+        "X-CMC_PRO_API_KEY": apiKey,
+      },
+    });
+    response.body.data.forEach((coin) => {
+      coinsMarketCaps[coin.id] = coin.quote.USD.market_cap;
+      coinsIds.push(coin.id);
+    });
+
+    // divide total coins (IDs) to retrieve in chunks to create multiple calls to the coin metadata endpoint
+    // this because coins Ids are passed to the endpoint as a query string param and query strings have a limit on their size/length
+    const subcallsCoinsIds = arrayTo2DArray1(coinsIds, coinsPerSubCall);
+
+    try {
+      for (subcallCoinsIds of subcallsCoinsIds) {
+        await delay(2500);
+        const response = await needle(
+          "get",
+          `${ENDPOINTS.COINMARKETCAP_COIN_INFO}${subcallCoinsIds.join(",")}`,
+          {
+            headers: {
+              "X-CMC_PRO_API_KEY": apiKey,
+            },
+          }
+        );
+
+        Object.keys(response.body.data).forEach((coinId) => {
+          coinsArray.push([
+            response.body.data[coinId].name,
+            coinsMarketCaps[coinId],
+            response.body.data[coinId].urls.website[0],
+          ]);
+        });
+      }
+    } catch (error) {
+      console.error(
+        `Problems while calling Coinmarketcap API coins details endpoint. Error: ${error}`
+      );
+      process.exit(1);
+    }
+  } catch (error) {
+    console.error(
+      "Problems while calling Coinmarketcap API coins listing endpoint. Error: " +
+        error
+    );
+    process.exit(1);
+  }
+
+  for (coin of coinsArray) {
+    try {
+      let coinDomainName = "";
+      if (coin[2]) {
+        const coinDomainSplit1 = coin[2].split(/(https:\/\/|http:\/\/)+/);
+        const coinDomainSplit2 =
+          coinDomainSplit1[coinDomainSplit1.length - 1].split(/(\/)+/);
+        coinDomainName = coinDomainSplit2[0].replace("www.", "");
+      }
+
+      fs.appendFileSync(DB_PATH + "/coinmarketcaps.txt", coinDomainName + "\n");
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  process.exit(0);
+}
+
+const target = process.argv[2];
+
+switch (target) {
+  case "tranco":
+    updateTrancoList();
+    break;
+  case "coinmarketcap":
+    updateCoinmarketcapList();
+    break;
+  default:
+    console.log('You need to specify either: "tranco" OR "coinmarketcap"');
+    break;
+}
