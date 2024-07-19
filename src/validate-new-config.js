b2b8c1d345d10a5835dbae8b235090d60ceba688
Nikita Varabei <nVarabei@gmail.com>
2024-07-19 16:08:52 -0400

    add mountaintop.finance allowlisted site to blocklist to test, and update validate-new-config.js with chainpatrol allowlist check

--- src/validate-new-config.js
+++ src/validate-new-config.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 
-const { readFileSync } = require('fs');
+const { readFileSync } = require("fs");
 
 const exitWithUsage = (exitCode) => {
-  console.error(`Usage: ${
-    process.argv.slice(0,2).join(' ')
-  } baseconfig.json newconfig.json`);
-  console.error('Compares diff between base and new config, validating invariants');
+  console.error(
+    `Usage: ${process.argv
+      .slice(0, 2)
+      .join(" ")} baseconfig.json newconfig.json`
+  );
+  console.error(
+    "Compares diff between base and new config, validating invariants"
+  );
   process.exit(exitCode);
 };
 
@@ -19,25 +23,68 @@ if (process.argv.length !== 4) {
   exitWithUsage(1);
 }
 
-try {
-  const [baseConfig, newConfig] = process.argv.slice(2).map(p => JSON.parse(readFileSync(p)));
-  {
-    // 1. Fuzzylist is remove-only
-    let result;
-    if (result = newConfig.fuzzylist.find(h => !baseConfig.fuzzylist.includes(h))) {
-      exitWithFail(`unexpected fuzzylist entry "${result}"`);
+(async () => {
+  try {
+    console.log("starting validate-new-config");
+    const [baseConfig, newConfig] = process.argv
+      .slice(2)
+      .map((p) => JSON.parse(readFileSync(p)));
+    {
+      // 1. Fuzzylist is remove-only
+      let result;
+      if (
+        (result = newConfig.fuzzylist.find(
+          (h) => !baseConfig.fuzzylist.includes(h)
+        ))
+      ) {
+        exitWithFail(`unexpected fuzzylist entry "${result}"`);
+      }
     }
-  }
-  {
-    // 2. Fuzzy-tolerance is strictly bounded to not grow
-    if (!(newConfig.tolerance <= baseConfig.tolerance)) {
-      exitWithFail(`new tolerance ${newConfig.tolerance} must be <= old tolerance ${oldConfig.tolerance}`);
+    {
+      // 2. Fuzzy-tolerance is strictly bounded to not grow
+      if (!(newConfig.tolerance <= baseConfig.tolerance)) {
+        exitWithFail(
+          `new tolerance ${newConfig.tolerance} must be <= old tolerance ${oldConfig.tolerance}`
+        );
+      }
     }
-  }
-  // TODO: enable once lists are sorted in `config.json`
-  /*
+    {
+      // Check against ChainPatrol Allowlist API
+      // Get only the blocklist additions in newConfig
+      const blocklistAdditions = newConfig.blacklist.filter(
+        (h) => !baseConfig.blacklist.includes(h)
+      );
+
+      const chainpatrolAPIoptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: '{"content":"<string>"}',
+      };
+
+      console.log(
+        "Running blocklist additions against ChainPatrol Allowlist API",
+        blocklistAdditions
+      );
+
+      // Check that they are not in the allowlist
+      for (const blocklistAddition of blocklistAdditions) {
+        //call chainpatrol api asset-check
+        const response = await fetch(
+          "https://app.chainpatrol.io/api/v2/asset/check",
+          chainpatrolAPIoptions
+        );
+        const data = await response.json();
+        if (data.status === "ALLOWED") {
+          exitWithFail(
+            `new blocklist entry "${blocklistAddition}" is in the allowlist`
+          );
+        }
+      }
+    }
+    // TODO: enable once lists are sorted in `config.json`
+    /*
   {
-    // 3. Entries are sorted alphabetically
+    // 4. Entries are sorted alphabetically
     ['fuzzylist','blacklist','whitelist'].forEach(listName => {
       if(!deepEqual(newConfig[listName], [...newConfig[listName]].sort())) {
         exitWithFail(`${listName} not sorted`);
@@ -45,6 +92,7 @@ try {
     });
   }
   */
-} catch (err) {
-  exitWithFail(err.message || err);
-}
+  } catch (err) {
+    exitWithFail(err.message || err);
+  }
+})();
